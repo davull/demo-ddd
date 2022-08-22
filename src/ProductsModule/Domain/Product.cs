96b02a0bfd40a0fca31c834be4004bf4d96ffb22
David Ullrich <david.ullrich@ullrich-software.at>
2022-08-22 22:16:49 +0200

    Product

---
+++ src/ProductsModule/Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿using SharedKernel;
+
+namespace ProductsModule.Domain;
+
+using ProductIdentifier = Identifier<string>;
+
+public record Product(
+    ProductIdentifier Id,
+    string Name,
+    string Description,
+    Money Price);
